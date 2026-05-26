d6a2841c561e586b099087a18da534ab6f66b3dd
daning1212 <sadwar1001@gmail.com>
2026-05-26 04:08:14 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,21 @@
-
 const reveals = document.querySelectorAll('.reveal');
 
-function revealOnScroll(){
+function revealElements(){
+
     const trigger = window.innerHeight * 0.85;
 
     reveals.forEach(el=>{
+
         const top = el.getBoundingClientRect().top;
 
         if(top < trigger){
             el.classList.add('active');
         }
+
     });
+
 }
 
-window.addEventListener('scroll', revealOnScroll);
-revealOnScroll();
+window.addEventListener('scroll', revealElements);
 
-window.addEventListener('scroll', ()=>{
-    const nav = document.querySelector('.navbar');
-
-    if(window.scrollY > 50){
-        nav.style.background = 'rgba(0,0,0,.75)';
-    }else{
-        nav.style.background = 'rgba(0,0,0,.45)';
-    }
-});
-
-console.log('HealingArty loaded.');
+revealElements();
